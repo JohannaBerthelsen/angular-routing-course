b1c3516f4f9f7d1739f6cc2c1da8a70005dc5254
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-17 19:09:48 +0200

    routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { NavigationStart, Router, RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.html',
-    styleUrls: ['./app.scss'],
-    imports: [HeaderComponent, HomeComponent],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+  imports: [HeaderComponent, RouterModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
+export class App implements OnInit {
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        console.log('Navigation Start');
+      }
+    });
+  }
+
+  onActivate(event: unknown) {
+    console.log('on activate', event);
+  }
 }
