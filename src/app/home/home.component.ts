7f94a49290426f69df5a35112685c3b862f4981f
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-19 16:26:35 +0200

    tracking

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { SubSectionComponent } from './sub-section/sub-section.component';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    imports: [HeroSectionComponent, SubSectionComponent],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  imports: [HeroSectionComponent, SubSectionComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor() {
+    console.log('In constructor');
+  }
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
 }
