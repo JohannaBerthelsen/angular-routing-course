ab94f83f044e2105127f62626592373d4979803b
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-19 17:59:45 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,39 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
+
+export enum ROUTER_TOKENS {
+  HOME = 'home',
+  SHOP = 'shop',
+  CONTACT = 'contact',
+  ABOUT = 'about',
+}
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: '',
+    redirectTo: ROUTER_TOKENS.HOME,
+    pathMatch: 'full',
+  },
+  {
+    path: ROUTER_TOKENS.HOME,
     component: HomeComponent,
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: ROUTER_TOKENS.SHOP,
+    component: ProductsComponent,
+  },
+  {
+    path: ROUTER_TOKENS.CONTACT,
+    component: ContactComponent,
+  },
+  {
+    path: ROUTER_TOKENS.ABOUT,
+    component: AboutComponent,
   },
   {
     path: '**',
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,19 +5,29 @@ import { MatMenuItem, MatMenuTrigger, MatMenu } from '@angular/material/menu';
 import { MatButton } from '@angular/material/button';
 import { CartService } from '../services/cart.service';
 import { AuthService } from '../services/auth.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ROUTER_TOKENS } from '../app.routes';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    imports: [MatMenuItem, MatButton, MatMenuTrigger, MatMenu]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [
+    MatMenuItem,
+    MatButton,
+    MatMenuTrigger,
+    MatMenu,
+    RouterLink,
+    RouterLinkActive,
+  ],
 })
 export class HeaderComponent {
+  readonly ROUTER_TOKENS = ROUTER_TOKENS;
   protected readonly pieService = inject(PieService);
   protected readonly authService = inject(AuthService);
   protected cartService = inject(CartService);
 
-  changeCategory(category: Category){
+  changeCategory(category: Category) {
     this.pieService.setSelectedCategory(category);
   }
 }
